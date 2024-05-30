b120d571fb7582f538c24e2f435ec4ac0c66d40d
naranjaFanta <eduardo.tecmet@hotmail.com>
2024-05-30 18:40:05 -0300

    prueba

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const app = express()
  app.use(express.urlencoded({extended:true}))
 
 // app.use("/api",route)
-
+//
  app.listen(PORT,() => console.log(`servidor listo en puerto ${PORT}`))
  app.on("error",(error) => console.log(`error !!!!!!!!: ${error}`))
