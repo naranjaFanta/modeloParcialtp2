d4f0c306f49cd1ca851a0965cb837848cfc3078b
naranjaFanta <eduardo.tecmet@hotmail.com>
2024-05-30 18:35:24 -0300

    dejo servidor listo con expres

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+// import route from './routes/poke.routes.js'
+const app = express()
+ const PORT = 8080
+
+ app.use(express.json())
+ app.use(express.urlencoded({extended:true}))
+
+// app.use("/api",route)
+
+ app.listen(PORT,() => console.log(`servidor listo en puerto ${PORT}`))
+ app.on("error",(error) => console.log(`error !!!!!!!!: ${error}`))
